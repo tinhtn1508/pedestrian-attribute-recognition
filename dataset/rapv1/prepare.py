1d2af991a90df3f31b0ef720473c3393f0a762c3
Tinh Nguyen <tinhtn1508@gmail.com>
2021-03-06 23:03:11 +0700

    update rapv1

--- dataset/rapv1/prepare.py
+++ dataset/rapv1/prepare.py
@@ -7,6 +7,110 @@ import argparse
 from sklearn.model_selection import train_test_split
 from loguru import logger
 
+attributeName = ['Female',
+                'AgeLess16',
+                'Age17-30',
+                'Age31-45',
+                'BodyFat',
+                'BodyNormal',
+                'BodyThin',
+                'Customer',
+                'Clerk',
+                'BaldHead',
+                'LongHair',
+                'BlackHair',
+                'Hat',
+                'Glasses',
+                'Muffler',
+                'Shirt',
+                'Sweater',
+                'Vest',
+                'TShirt',
+                'Cotton',
+                'Jacket',
+                'Suit-Up',
+                'Tight',
+                'ShortSleeve',
+                'LongTrousers',
+                'Skirt',
+                'ShortSkirt',
+                'Dress',
+                'Jeans',
+                'TightTrousers',
+                'LeatherShoes',
+                'SportShoes',
+                'Boots',
+                'ClothShoes',
+                'CasualShoes',
+                'Backpack',
+                'SSBag',
+                'HandBag',
+                'Box',
+                'PlasticBag',
+                'PaperBag',
+                'HandTrunk',
+                'OtherAttchment',
+                'Calling',
+                'Talking',
+                'Gathering',
+                'Holding',
+                'Pusing',
+                'Pulling',
+                'CarryingbyArm',
+                'CarryingbyHand']
+
+lossWeight = [0.311434,
+            0.009980,
+            0.430011,
+            0.560010,
+            0.144932,
+            0.742479,
+            0.097728,
+            0.946303,
+            0.048287,
+            0.004328,
+            0.189323,
+            0.944764,
+            0.016713,
+            0.072959,
+            0.010461,
+            0.221186,
+            0.123434,
+            0.057785,
+            0.228857,
+            0.172779,
+            0.315186,
+            0.022147,
+            0.030299,
+            0.017843,
+            0.560346,
+            0.000553,
+            0.027991,
+            0.036624,
+            0.268342,
+            0.133317,
+            0.302465,
+            0.270891,
+            0.124059,
+            0.012432,
+            0.157340,
+            0.018132,
+            0.064182,
+            0.028111,
+            0.042155,
+            0.027558,
+            0.012649,
+            0.024504,
+            0.294601,
+            0.034099,
+            0.032800,
+            0.091812,
+            0.024552,
+            0.010388,
+            0.017603,
+            0.023446,
+            0.128917]
+
 def generate(args):
     rapv1_decription = loadmat(args.annotation_file)
     rapv1_decription = rapv1_decription['RAP_annotation']
@@ -15,32 +119,29 @@ def generate(args):
     dataset.description = 'rapv1'
     dataset.reorder = 'group_order'
     dataset.root = args.data_dir
+    dataset.attr_name =  attributeName
+    dataset.loss_weight = np.array(lossWeight)
 
-    dataset.image_name = [rapv1_decription[0][0][5][i][0][0] for i in range(41585)]
-    raw_attr_name = [rapv1_decription[0][0][3][i][0][0] for i in range(92)]
-    raw_label = rapv1_decription[0][0][1]
-    dataset.label = np.array(raw_label[:, np.array(range(51))])
-    dataset.attr_name = [raw_attr_name[i] for i in range(51)]
+    labels = []
+    imgName = []
+    with open('train.txt', 'r') as f:
+        for row in f.readlines():
+            token = row[:-1].split(' ')
+            imgName.append(token[0])
+            label = [int(i) for i in token[1:]]
+            labels.append(label)
+    with open('test.txt', 'r') as f:
+        for row in f.readlines():
+            token = row[:-1].split(' ')
+            imgName.append(token[0])
+            label = [int(i) for i in token[1:]]
+            labels.append(label)
 
-    train = []
-    test = []
-    for idx in range(5):
-        _train = rapv1_decription[0][0][0][idx][0][0][0][0][0, :] - 1
-        _test = rapv1_decription[0][0][0][idx][0][0][0][1][0, :] - 1
-        train.append(_train)
-        test.append(_test)
-    train = np.concatenate(train)
-    test = np.concatenate(test)
-    data = np.concatenate([train, test])
-
+    dataset.image_name = imgName
+    dataset.label = np.array(labels)
     dataset.partition = EasyDict()
-    train_index, test_index = train_test_split(data, shuffle=args.shuffle, train_size=args.train_rate, random_state=42)
-    dataset.partition.train = train_index
-    dataset.partition.test = test_index
-
-    count_label = np.count_nonzero(dataset.label, axis=0)
-    loss_weight = [i/41585 for i in count_label]
-    dataset.loss_weight = np.array(loss_weight)
+    dataset.partition.train = np.arange(0, 33268)
+    dataset.partition.test = np.arange(33268, 41585)
 
     with open(os.path.join(args.save_dir, 'rapv1_description.pkl'), 'wb+') as f:
         pickle.dump(dataset, f)
