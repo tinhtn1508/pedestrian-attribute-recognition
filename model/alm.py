555febe27b4006f8dd98a179e9d9dec908905482
Tinh Nguyen <tinhtn1508@gmail.com>
2021-03-12 00:14:07 +0700

    add soft attention

--- model/alm.py
+++ model/alm.py
@@ -30,21 +30,117 @@ def inception_iccv(pretrained=True, debug=False, **kwargs):
         model.load_state_dict(model_dict)
     return model
 
+class ConvBlock(nn.Module):
+    """Basic convolutional block.
+    
+    convolution + batch normalization + relu.
+    Args:
+        in_c (int): number of input channels.
+        out_c (int): number of output channels.
+        k (int or tuple): kernel size.
+        s (int or tuple): stride.
+        p (int or tuple): padding.
+    """
+
+    def __init__(self, in_c, out_c, k, s=1, p=0):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2d(in_c, out_c, k, stride=s, padding=p)
+        self.bn = nn.BatchNorm2d(out_c)
+
+    def forward(self, x):
+        return F.relu(self.bn(self.conv(x)))
 
 class ChannelAttn(nn.Module):
     def __init__(self, in_channels, reduction_rate=16):
         super(ChannelAttn, self).__init__()
-        assert in_channels%reduction_rate == 0
-        self.conv1 = nn.Conv2d(in_channels, in_channels // reduction_rate, kernel_size=1, stride=1, padding=0)
-        self.conv2 = nn.Conv2d(in_channels // reduction_rate, in_channels, kernel_size=1, stride=1, padding=0)
+        assert in_channels % reduction_rate == 0
+        self.conv1 = ConvBlock(in_channels, in_channels // reduction_rate, 1)
+        self.conv2 = ConvBlock(in_channels // reduction_rate, in_channels, 1)
 
     def forward(self, x):
         # squeeze operation (global average pooling)
         x = F.avg_pool2d(x, x.size()[2:])
         # excitation operation (2 conv layers)
-        x = F.relu(self.conv1(x))
+        x = self.conv1(x)
         x = self.conv2(x)
-        return torch.sigmoid(x)
+        return x
+
+class SpatialAttn(nn.Module):
+    def __init__(self):
+        super(SpatialAttn, self).__init__()
+        self.conv1 = ConvBlock(1, 1, 3, s=2, p=1)
+        self.conv2 = ConvBlock(1, 1, 1)
+
+    def forward(self, x):
+        # global cross-channel averaging
+        x = x.mean(1, keepdim=True)
+        # 3-by-3 conv
+        x = self.conv1(x)
+        # bilinear resizing
+        x = F.upsample(
+            x, (x.size(2) * 2, x.size(3) * 2),
+            mode='bilinear',
+            align_corners=True
+        )
+        # scaling conv
+        x = self.conv2(x)
+        return x
+
+class SoftAttn(nn.Module):
+    """Soft Attention (Sec. 3.1.I)
+    
+    Aim: Spatial Attention + Channel Attention
+    
+    Output: attention maps with shape identical to input.
+    """
+
+    def __init__(self, in_channels):
+        super(SoftAttn, self).__init__()
+        self.spatial_attn = SpatialAttn()
+        self.channel_attn = ChannelAttn(in_channels)
+        self.conv = ConvBlock(in_channels, in_channels, 1)
+
+    def forward(self, x):
+        y_spatial = self.spatial_attn(x)
+        y_channel = self.channel_attn(x)
+        y = y_spatial * y_channel
+        y = torch.sigmoid(self.conv(y))
+        return y
+
+class HardAttn(nn.Module):
+    def __init__(self, in_channels):
+        super(HardAttn, self).__init__()
+        self.fc = nn.Linear(in_channels, 4 * 2)
+        self.init_params()
+
+    def init_params(self):
+        self.fc.weight.data.zero_()
+        self.fc.bias.data.copy_(
+            torch.tensor(
+                [0, -0.75, 0, -0.25, 0, 0.25, 0, 0.75], dtype=torch.float
+            )
+        )
+
+    def forward(self, x):
+        # squeeze operation (global average pooling)
+        x = F.avg_pool2d(x, x.size()[2:]).view(x.size(0), x.size(1))
+        # predict transformation parameters
+        theta = torch.tanh(self.fc(x))
+        theta = theta.view(-1, 4, 2)
+        return theta
+
+class HarmAttn(nn.Module):
+    """Harmonious Attention (Sec. 3.1)"""
+
+    def __init__(self, in_channels):
+        super(HarmAttn, self).__init__()
+        self.soft_attn = SoftAttn(in_channels)
+        self.hard_attn = HardAttn(in_channels)
+
+    def forward(self, x):
+        y_soft_attn = self.soft_attn(x)
+        theta = self.hard_attn(x)
+        return y_soft_attn, theta
 
 class VisualAttn(nn.Module):
     def __init__(self, in_channels, reduction_rate, num_classes):
@@ -79,17 +175,17 @@ class SpatialTransformBlock(nn.Module):
 
         self.gap_list = nn.ModuleList()
         self.fc_list = nn.ModuleList()
-        # self.att_list = nn.ModuleList()
-        self.visual_att = nn.ModuleList()
+        self.att_list = nn.ModuleList()
+        # self.visual_att = nn.ModuleList()
         self.conf_weight = nn.ModuleList()
         self.stn_list = nn.ModuleList()
         for i in range(self.num_classes):
             self.gap_list.append(nn.AvgPool2d((pooling_size, pooling_size//2), stride=1, padding=0, ceil_mode=True, count_include_pad=True))
-            self.fc_list.append(nn.Linear(num_classes, 1))
-            # self.att_list.append(ChannelAttn(channels))
-            self.visual_att.append(VisualAttn(channels, 16, num_classes))
+            self.fc_list.append(nn.Linear(channels, 1))
+            self.att_list.append(SoftAttn(channels))
+            # self.visual_att.append(VisualAttn(channels, 16, num_classes))
             self.conf_weight.append(ConfidenceWeighting(channels, num_classes))
-            self.stn_list.append(nn.Linear(num_classes, 4))
+            self.stn_list.append(nn.Linear(channels, 4))
 
     def stn(self, x, theta):
         grid = F.affine_grid(theta, x.size())
@@ -109,8 +205,8 @@ class SpatialTransformBlock(nn.Module):
         pred_list = []
         bs = features.size(0)
         for i in range(self.num_classes):
-            # stn_feature = features * self.att_list[i](features) + features
-            stn_feature = self.visual_att[i](features)*self.conf_weight[i](features)
+            stn_feature = features * self.att_list[i](features) + features
+            # stn_feature = self.visual_att[i](features)*self.conf_weight[i](features)
             theta_i = self.stn_list[i](F.avg_pool2d(stn_feature, stn_feature.size()[2:]).view(bs,-1)).view(-1,4)
             theta_i = self.transform_theta(theta_i, i)
 
